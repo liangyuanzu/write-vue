65315aab681aa52166d04ab693737d42dd74eda1
liangyuanzu <liangyuanzu@gmail.com>
2021-03-04 23:20:30 +0800

    feat: 监听数据变化

--- Vue/js/lue.js
+++ Vue/js/lue.js
@@ -36,6 +36,8 @@ class Lue {
     this.$data = options.data;
     // 2.根据指定的区域和数据去编译渲染界面
     if (this.$el) {
+      // 给外界传入的所有数据都添加get/set方法
+      new Observer(this.$data);
       new Compiler(this);
     }
   }
@@ -95,5 +97,46 @@ class Compiler {
     if (reg.test(content)) {
       CompilerUtil["content"](text, content, this.vm);
     }
+  }
+}
+
+class Observer {
+  constructor(data) {
+    this.observer(data);
+  }
+  observer(obj) {
+    if (obj && typeof obj === "object") {
+      for (const key in obj) {
+        this.defineReactive(obj, key, obj[key]);
+      }
+    }
+  }
+  defineReactive(obj, attr, value) {
+    // 如果属性的取值又是一个对象, 那么也需要给这个对象的所有属性添加get/set方法
+    this.observer(value);
+    /**
+     * Object.defineProperty(obj, prop, descriptor)
+     * 直接在一个对象上定义一个新属性，或者修改一个对象的现有属性， 并返回这个对象
+     * @param {object} obj  要定义属性的对象。
+     * @param  prop  要定义或修改的属性的名称或 Symbol 。
+     * @param {object} descriptor  要定义或修改的属性描述符。
+     *    @param {any} value  属性对应的值,可以是任意类型的值，默认为undefined
+     *    @param {boolean} writable  属性的值是否可以被重写。默认为false。
+     *    @param {boolean} enumerable  目标属性是否可以被枚举。默认为false。
+     *    @param {boolean} configurable  目标属性是否可以被删除或是否可以再次修改特性。默认为false。
+     */
+    Object.defineProperty(obj, attr, {
+      // 当使用了getter或setter方法，不允许使用writable和value这两个属性
+      get: () => value, //当获取值的时候触发的函数
+      set: (newValue) => {
+        //当设置值的时候触发的函数
+        if (value !== newValue) {
+          // 如果给属性赋值的新值又是一个对象, 那么也需要给这个对象的所有属性添加get/set方法
+          this.observer(newValue);
+          value = newValue;
+          console.log("监听到数据的变化, 需要去更新UI");
+        }
+      },
+    });
   }
 }
