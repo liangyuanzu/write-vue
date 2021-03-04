5fbb905cedad3ca66b5893c203fc72a8f284058d
liangyuanzu <liangyuanzu@gmail.com>
2021-03-04 18:29:46 +0800

    feat: 提取元素到内存

--- Vue/js/lue.js
+++ Vue/js/lue.js
@@ -20,5 +20,21 @@ class Lue {
 class Compiler {
   constructor(vm) {
     this.vm = vm;
+    // 1. 将网页上的元素放到内存中
+    const fragment = this.node2fragment(this.vm.$el);
+    console.log(fragment);
+  }
+  node2fragment(app) {
+    // 1. 创建一个空的文档碎片对象
+    const newFragment = document.createDocumentFragment();
+    // 2. 遍历循环取到每一个元素
+    let node = app.firstChild;
+    while (node) {
+      // 注意: 主要将元素添加到文档碎片对象中, 那么这个元素就会自动从网页中消失
+      newFragment.appendChild(node);
+      node = app.firstChild;
+    }
+    // 3. 返回存储了所有元素的文档碎片对象
+    return newFragment;
   }
 }
