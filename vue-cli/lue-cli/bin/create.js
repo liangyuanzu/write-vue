db7c597c7664778b5a6f342be657e608011530ea
liangyuanzu <liangyuanzu@gmail.com>
2021-03-06 14:56:00 +0800

    feat: 拉取模板名称

--- vue-cli/lue-cli/bin/create.js
+++ vue-cli/lue-cli/bin/create.js
@@ -1,3 +1,15 @@
-module.exports = (projectName) => {
-  console.log("create", projectName);
+const axios = require("axios");
+const ora = require("ora");
+
+const getTemplateNames = async () => {
+  return await axios.get("https://api.github.com/orgs/it666-com/repos");
+};
+
+module.exports = async (projectName) => {
+  // 1.下载指定模板
+  const spinner = ora("Loading unicorns").start("downloading template names"); // loading
+  const { data } = await getTemplateNames();
+  const templateNames = data.map((i) => i.name);
+  spinner.succeed("download template names successfully");
+  console.log(templateNames);
 };
