f635cfce40e97068d07714cc5ae3649fb741ee1d
liangyuanzu <liangyuanzu@gmail.com>
2021-03-06 15:37:32 +0800

    feat: 拉取版本信息

--- vue-cli/lue-cli/bin/create.js
+++ vue-cli/lue-cli/bin/create.js
@@ -3,21 +3,42 @@ const ora = require("ora"); // loading
 const inquirer = require("inquirer"); // 实现用户交互
 
 const getTemplateNames = async () => {
-  return await axios.get("https://api.github.com/orgs/it666-com/repos");
+  const { data } = await axios.get("https://api.github.com/orgs/it666-com/repos");
+  return data;
+};
+
+const getTemplateTags = async (name) => {
+  const { data } = await axios.get(`https://api.github.com/repos/it666-com/${name}/tags`);
+  return data;
 };
 
 module.exports = async (projectName) => {
   // 1.拉取所有模板名称
-  const spinner = ora("Loading unicorns").start("downloading template names");
-  const { data } = await getTemplateNames();
-  const templateNames = data.map((i) => i.name);
+  const spinner = ora("Loading").start("downloading template names");
+  const names = await getTemplateNames();
+  const templateNames = names.map((i) => i.name);
   spinner.succeed("download template names successfully");
-  // 2.让用户选择指定模板名称
+
+  // 2.选择指定模板名称
   const { currentTemplateName } = await inquirer.prompt({
     name: "currentTemplateName", // 存储当前问题回答的变量
     type: "list", // 提问的类型
     choices: templateNames, // 列表选项
     message: "请选择要使用哪个模板来创建项目", // 问题的描述
   });
-  console.log(currentTemplateName);
+
+  // 3.获取版本号
+  spinner.start("downloading template tags");
+  const tags = await getTemplateTags(currentTemplateName);
+  const templateTags = tags.map((i) => i.name);
+  spinner.succeed("download template tags successfully");
+
+  // 4.选择版本
+  const { currentTemplateTag } = await inquirer.prompt({
+    name: "currentTemplateTag",
+    type: "list",
+    choices: templateTags,
+    message: "请选择要使用哪个版本来创建项目",
+  });
+  console.log(currentTemplateName, currentTemplateTag);
 };
