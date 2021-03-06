4960a1985126cefcdcbe3041b8d564e031c7768e
liangyuanzu <liangyuanzu@gmail.com>
2021-03-06 14:49:10 +0800

    feat: 下载指定模板

--- vue-cli/lue-cli/bin/create.js
+++ vue-cli/lue-cli/bin/create.js
@@ -1,3 +1,15 @@
-module.exports = (projectName) => {
-  console.log("create", projectName);
+const axios = require("axios");
+const ora = require("ora");
+
+const getTemplateName = async () => {
+  return await axios.get("https://api.github.com/orgs/it666-com/repos");
+};
+
+module.exports = async (projectName) => {
+  // 1.下载指定模板
+  const spinner = ora("downloading template names").start(); // loading
+  const { data } = await getTemplateName();
+  const templateNames = data.map((i) => i.name);
+  spinner.succeed("download template names successfully");
+  console.log(templateNames);
 };
