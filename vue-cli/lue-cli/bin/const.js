da6fb9ce1f50f2d710358d037c0056cfd4f69178
liangyuanzu <liangyuanzu@gmail.com>
2021-03-06 16:03:37 +0800

    feat: 获取下载目录

--- vue-cli/lue-cli/bin/const.js
+++ vue-cli/lue-cli/bin/const.js
@@ -1,5 +1,8 @@
 const { version } = require("../package.json");
 
+const currentPlatformKey = process.platform === "win32" ? "USERPROFILE" : "HOME";
+const downloadDirPath = `${process.env[currentPlatformKey]}\\.lue-template`;
 module.exports = {
   version,
+  downloadDirPath,
 };
